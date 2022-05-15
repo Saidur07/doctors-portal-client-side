7d57e05f4aa5f6a04008b0fb98e3caef5b8bea22
Saidur-07 <md.saidurrahman.bhola2007@gmail.com>
2022-05-15 13:36:57 +0600

    Lazy Load image componenet added

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,12 +1,19 @@
 import React from "react";
 import chair from "../../assets/images/chair.png";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import PrimaryButton from "../Shared/PrimaryButton";
 
 const Banner = () => {
   return (
     <div className="hero min-h-[70vh]">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" />
+        {/* <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" /> */}
+        <LazyLoadImage
+          effect="blur"
+          className="max-w-sm rounded-lg shadow-2xl"
+          src={chair}
+        />
         <div>
           <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
           <p className="py-6">
