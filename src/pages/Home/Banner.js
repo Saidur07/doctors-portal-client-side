bcd829a7a905fbcfcf463c28f1c3a2cbef54dbc2
Saidur-07 <md.saidurrahman.bhola2007@gmail.com>
2022-05-20 16:36:53 +0600

    Bug fixes

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,6 +1,5 @@
 import React from "react";
 import chair from "../../assets/images/chair.png";
-import loader from "../../assets/images/loader.gif";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import PrimaryButton from "../Shared/PrimaryButton";
@@ -9,12 +8,10 @@ const Banner = () => {
   return (
     <div className="hero min-h-[70vh]">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        {/* <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" /> */}
         <LazyLoadImage
           effect="blur"
           className="max-w-sm rounded-lg shadow-2xl"
           src={chair}
-          placeholderSrc={loader}
         />
         <div>
           <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
